ae422e8d95ce11cee48391107d7ce8aaa9f88a9a
Pradeep Singh Rajpoot <pradeep.s.rajpoot@gmail.com>
2024-04-22 21:39:29 +0530

    glasses integrated in AR canvas

---
+++ Assets/Scripts/GlassesButtonClicked.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassesButtonClicked : MonoBehaviour
+{
+    public void loadGlasses(string glassTag)
+    {
+        Debug.Log(glassTag);
+    }
+}
